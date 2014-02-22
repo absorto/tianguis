44704b18a742b05ae96745ca9446adc741710d0a
Rodrigo García <absorto@sdf.org>
2014-02-22 17:07:21 -0600

    modelo para tiendas fuera de la vendimia, formulario con ofertas como inline de una vendimia, status de una vendimia

--- tianguis/vendimia/admin.py
+++ tianguis/vendimia/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 
 from vendimia.models import *
 
+
+class OfertaInline(admin.TabularInline):
+    model = Oferta
+    extra = 9
+
+class VendimiaAdmin(admin.ModelAdmin):
+    inlines = [OfertaInline]
+
 admin.site.register( Producto )
-admin.site.register( Vendimia )
-admin.site.register( Oferta )
+admin.site.register( Vendimia, VendimiaAdmin )
 admin.site.register( Pedido )
+admin.site.register( Tienda )
 
--- tianguis/vendimia/models.py
+++ tianguis/vendimia/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 class Producto(models.Model):
     nombre      = models.CharField(max_length=200)
@@ -10,15 +10,40 @@ class Producto(models.Model):
     def __unicode__(self):
         return self.nombre
 
+
+class Tienda(models.Model):
+    nombre    = models.CharField('Local donde se entregan pedidos', max_length=200)
+    ubicacion = models.URLField('URL de un mapa de la ubicacion del local', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.nombre
+
+    
 class Vendimia(models.Model):
     cierre = models.DateTimeField('Fecha de cierre')
     entrega_inicio = models.DateTimeField('Inicia lapso de entregas')
     entrega_fin    = models.DateTimeField('Termina lapso de entregas')
-    local          = models.CharField('Local donde se entregan pedidos', max_length=200)
-    ubicacion      = models.URLField('URL de un mapa de la ubicacion del local', null=True, blank=True)
+
+    tienda = models.ForeignKey(Tienda)
+
+    def status(self):
+        now = datetime.datetime.now(self.cierre.tzinfo)
+        
+        if now < self.cierre:
+            status = ["recibiendo pedidos"]
+        else:
+            status = ["cerrada"]
+
+        if now >= self.entrega_inicio and now <= self.entrega_fin:
+            status.append( "entregando" )
+
+        if now > self.entrega_fin:
+            status.append( "finalizada" )
+
+        return " | ".join(status)
 
     def __unicode__(self):
-        return self.local
+        return "%s [%s]" % (self.tienda.nombre, self.status())
 
 class Oferta(models.Model):
     producto = models.ForeignKey(Producto)
