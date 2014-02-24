4033a3fd1360b2c95329446619e386311f55d3ff
Rodrigo García <absorto@sdf.org>
2014-02-23 18:22:53 -0600

    convierto el formulario de registrar la venta en una tabla, al postear la vista imprime el querystring a la consola
    quito vistas ejemplo, tambien del urls

--- tianguis/vendimia/urls.py
+++ tianguis/vendimia/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'adios/', 'vendimia.views.adios'),
-    url(r'/', 'vendimia.views.hola'),
 
 )
--- tianguis/vendimia/views.py
+++ tianguis/vendimia/views.py
@@ -6,20 +6,11 @@ from django.shortcuts import render
 
 from vendimia.models import *
 
-
-
-def hola(request):
-    return render_to_response('seccion_a.html', {'saludo': "hola como estas"})
-
-def adios(request):
-    return render_to_response('seccion_a.html', {'saludo': "ba bai"})
-
+import pprint
 
 @staff_member_required
 def gran_pedido(request, vendimia_id=None):
     v = Vendimia.objects.get( id = vendimia_id )
-    import pprint
-    pprint.pprint(v.gran_pedido())
     return render_to_response('gran_pedido.html',
                               context_instance=RequestContext(request,
                                                               {'title': 'Gran Pedido: %s' % v,
@@ -40,12 +31,17 @@ def pedidos_vendimia(request, vendimia_id=None):
 
 
 
+
 @staff_member_required
 def pedido_vendimia_user(request, vendimia_id, user_id):
     u = User.objects.get(id=user_id)
     v = Vendimia.objects.get(id=vendimia_id)
     pedidos = Pedido.objects.filter(oferta__vendimia=v,
                                     user=u)
+
+    if request.method == "POST":
+        pprint.pprint(request.POST)
+
     return render_to_response('pedidos_vendimia_usuario.html',
                               context_instance=RequestContext(request,
                                                               {'title': 'Orden: %s @ %s' % (u,v),
