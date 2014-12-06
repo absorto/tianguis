6aa7da89a137ce6f96f6e6174b44a4eb422f86a1
Rodrigo García <absorto@sdf.org>
2014-12-06 17:26:23 -0600

    del manual de flask falso login

--- tiangflask/views.py
+++ tiangflask/views.py
@@ -1,9 +1,14 @@
+from flask import Flask, session, redirect, url_for, escape, request, jsonify
 
-from flask import Flask, jsonify, make_response
 import pprint
 import sys
 
 app = Flask(__name__)
+
+
+############
+# anuncios #
+############
 
 @app.route('/anuncios/save', methods=['POST'])
 def anuncio_save():
@@ -14,6 +19,41 @@ def anuncio_save():
         return jsonify({ "status": "error",
                          "message": pprint.pformat(sys.exc_info()[0]) })
  
+
+
+
+
+###################
+# Falso login     #
+###################
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 
 
 if __name__ == '__main__':
