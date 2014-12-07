4067136c023d684a591e0a5d8feb7c8bde0572f8
Rodrigo García <absorto@sdf.org>
2014-12-06 20:45:32 -0600

    inicializa justo a tiempo e itemgrid

--- tiangflask/static/index.js
+++ tiangflask/static/index.js
@@ -9,7 +9,12 @@ w2utils.locale('bower_components/w2ui/locale/es-mx.json');
 // # /pedidos/mercado
 
 // widget configuration
+
 var config = {
+
+    ////////////////////
+    // layout general //
+    ////////////////////
     layout: {
         name: 'layout',
         padding: 0,
@@ -18,17 +23,21 @@ var config = {
             { type: 'main', minSize: 550, overflow: 'hidden' }
         ]
     },
+
+    /////////////
+    // sidebar, va en left //
+    /////////////
     sidebar: {
         name: 'sidebar',
         nodes: [ 
             { id: 'ofertas', text: 'Ofertas', group: true, expanded: true, nodes: [
-                { id: 'o_mias', text: 'Mías', img: 'icon-page', selected: true },
-                { id: 'o_inbox', text: 'Inbox', img: 'icon-page' },
+                { id: 'o_mias', text: 'Mías', img: 'icon-home', selected: true },
+                { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
                 { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' },
             ]},
             { id: 'pedidos', text: 'Pedidos', group: true, expanded: true, nodes: [
-                { id: 'p_mias', text: 'Mis pedidos', img: 'icon-page', selected: true },
-                { id: 'p_inbox', text: 'Inbox', img: 'icon-page' },
+                { id: 'p_mias', text: 'Mis pedidos', img: 'fa-home' },
+                { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
                 { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' },
             ]}
 
@@ -38,16 +47,21 @@ var config = {
                 case 'o_mias':
                     w2ui.layout.content('main', w2ui.grid1);
                     break;
-                case 'o_inbox':
+            case 'o_inbox':
+                    $().w2grid(config.grid2);
                     w2ui.layout.content('main', w2ui.grid2);
                     break;
             }
         }
     },
+
+
+    /////////////
+    // ofertas //
+    /////////////
     grid1: { 
         name: 'grid1',
         show: {
-            header  : true,
             toolbar : true,
             toolbarDelete: true,
             toolbarSave: true,
@@ -71,6 +85,7 @@ var config = {
                 editable: { type: 'checkbox' } 
             },
         ],
+        // sustituir por llamada al api
         records: [
             { recid: 1, titulo: 'puesto', desc: 'Tianguis el 100', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
             { recid: 2, titulo: 'A domicilio', desc: 'fruta y verdura otoñal', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
@@ -78,8 +93,17 @@ var config = {
         searches: [
             { type: 'date', field: 'vigencia', caption: 'Vigencia' }
         ],
-        onEdit: function() {openPopup()},
+        // falta onSave
+        onEdit: function() {
+            $().w2grid(itemgrid);
+            openPopup();
+        },
+    
     },
+
+    /////////////
+    // pedidos //
+    /////////////
     grid2: { 
         name: 'grid2',
                 show: {
@@ -118,29 +142,63 @@ var config = {
 
 
 
-$(function () {
-    // initialization
-    $('#main').w2layout(config.layout);
-    w2ui.layout.content('left', $().w2sidebar(config.sidebar));
-    w2ui.layout.content('main', $().w2grid(config.grid1));
-    // in memory initialization
-    $().w2grid(config.grid2);
-});
+var itemgrid = { 
+    name: 'itemgrid', 
+    show: {
+        toolbar: true,
+        toolbarDelete: true,
+        toolbarSave: true,
+    },
+    toolbar: {
+        items: [
+            { id: 'add', type: 'button', caption: 'Otro item', icon: 'w2ui-icon-plus' }
+        ],
+        onClick: function (event) {
+            if (event.target == 'add') {
+                w2ui.grid1.add({ recid: w2ui.itemgrid.records.length + 1 });
+            }
+        }
+    },
+
+    columns: [                
+        { field: 'text', caption: 'nombre', size: '120px', sortable: true, resizable: true, 
+          editable: { type: 'text' }
+        },
+        { field: 'desc', caption: 'descripci&oacute;n', size: '120px', sortable: true, resizable: true, 
+          editable: { type: 'text' }
+        },
+        { field: 'unidad', caption: 'unidad', size: '120px', sortable: true, resizable: true, 
+          editable: { type: 'text' }
+        },
+        { field: 'precio', caption: 'precio', size: '80px', sortable: true, resizable: true, render: 'money',
+          editable: { type: 'money' }
+        },
+    ],
+    toolbar: {
+        items: [
+            { id: 'add', type: 'button', caption: 'Nueva oferta', icon: 'w2ui-icon-plus' }
+        ],
+        onClick: function (event) {
+            if (event.target == 'add') {
+                w2ui.itemgrid.add({ recid: w2ui.itemgrid.records.length + 1 });
+            }
+        }
+    },
+    records: [
+    ]
+}
 
 
 
 function openPopup() {
     w2popup.open({
-        title   : 'Popup',
-        width   : 900,
+        title   : 'Items en la oferta',
+        width   : 600,
         height  : 600,
-        showMax : true,
-        body    : '<div id="popped" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
+        body    : '<div id="poplayout" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
         onOpen  : function (event) {
             event.onComplete = function () {
-                $('#w2ui-popup #popped').w2render('poplayout');
-//                w2ui.layout.content('left', w2ui.grid);
-//                w2ui.layout.content('main', w2ui.form);
+                $('#w2ui-popup #poplayout').w2render('itemgrid');
             };
         },
         onToggle: function (event) { 
@@ -150,3 +208,12 @@ function openPopup() {
         }
     });
 }
+
+
+
+$(function () {
+    // initialization
+    $('#main').w2layout(config.layout);
+    w2ui.layout.content('left', $().w2sidebar(config.sidebar));
+    w2ui.layout.content('main', $().w2grid(config.grid1));
+});
