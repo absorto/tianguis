fbb92d191ac8c0efe439568bd7809bbb84b05b42
Rodrigo García <absorto@sdf.org>
2014-12-06 22:16:33 -0600

    itemlist de pedido

--- tiangflask/static/index.js
+++ tiangflask/static/index.js
@@ -49,9 +49,13 @@ var config = {
                     w2ui.layout.content('main', w2ui.grid1);
                     break;
             case 'o_inbox':
-                    $().w2grid(config.grid2);
-                    w2ui.layout.content('main', w2ui.grid2);
+                    $().w2grid(config.o_inbox);
+                    w2ui.layout.content('main', w2ui.o_inbox);
+                break;
+            case 'o_mercado':
+                    w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
                     break;
+
             }
         }
     },
@@ -98,73 +102,69 @@ var config = {
 
         // abre lista de items
         onEdit: function(event) {
-            $().w2grid(itemgrid(event.recid));
-            openItemPopup(event.recid);
+            $().w2grid(o_itemgrid(event.recid));
+            openOfertaItemPopup(event.recid);
         },
     
     },
 
     /////////////
-    // pedidos //
+    // inbox //
     /////////////
-    grid2: { 
-        name: 'grid2',
+    o_inbox: { 
+        name: 'o_inbox',
                 show: {
-            header  : true,
-            toolbar : true,
-        },
-        toolbar: {
-            items: [
-                { id: 'add', type: 'button', caption: 'Nueva oferta', icon: 'w2ui-icon-plus' }
-            ],
-            onClick: function (event) {
-                if (event.target == 'add') {
-                    w2ui.grid1.add({ recid: w2ui.grid1.records.length + 1 });
-                }
-            }
-        },
-        columns: [
-            { field: 'titulo', caption: 'Título', size: '180px', editable: { type: 'text' } },
-            { field: 'desc', caption: 'Descripción', size: '180px', editable: { type: 'text' } },
-            { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true, editable: { type: 'date' },   },
-            { field: 'publicado', caption: 'Publicado', size: '100px', sortable: true, resizable: true, 
-                editable: { type: 'checkbox' } 
-            },
-        ],
-        records: [
-            { recid: 1, titulo: 'puesto', desc: 'Tianguis el 100', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
-            { recid: 2, titulo: 'A domicilio', desc: 'fruta y verdura otoñal', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
-        ],
+                    toolbar : true,
+                },
         searches: [
             { type: 'date', field: 'vigencia', caption: 'Vigencia' }
-        ]
+        ],
 
+        columns: [
+            { field: 'usuario', caption: 'Usuario', size: '180px' },
+            { field: 'titulo', caption: 'Título', size: '180px' },
+            { field: 'desc', caption: 'Descripción', size: '180px' },
+            { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true },
+        ],
+
+        onClick: function (event) {
+            $().w2grid(p_itemgrid(event.recid));
+            openPedidoItemPopup(event.recid);
+        },
+        
+        records: [
+            { recid: 1, usuario: "la granja", titulo: 'puesto', desc: 'Tianguis el 100', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
+            { recid: 2, usuario: "la nicolasa", titulo: 'A domicilio', desc: 'fruta y verdura otoñal', email: 'jdoe@gmail.com', vigencia: '4/3/2012' },
+        ],
+
+        
     }
 }
 
 
 
 
-function itemgrid(recid) {
+function o_itemgrid(recid) {
     return { 
-        name: 'itemgrid', 
+        name: 'o_itemgrid', 
         show: {
             toolbar: true,
             toolbarDelete: true,
             toolbarSave: true,
         },
 //        url: '/'+recid,
+
         toolbar: {
             items: [
                 { id: 'add', type: 'button', caption: 'Otro item', icon: 'w2ui-icon-plus' }
             ],
             onClick: function (event) {
                 if (event.target == 'add') {
-                    w2ui.grid1.add({ recid: w2ui.itemgrid.records.length + 1 });
+                    w2ui.o_itemgrid.add({ recid: w2ui.o_itemgrid.records.length + 1 });
                 }
             }
         },
-        
+
         columns: [                
             { field: 'text', caption: 'nombre', size: '120px', sortable: true, resizable: true, 
               editable: { type: 'text' }
@@ -179,23 +179,41 @@ function itemgrid(recid) {
               editable: { type: 'money' }
             },
         ],
-        toolbar: {
-            items: [
-                { id: 'add', type: 'button', caption: 'otro item', icon: 'w2ui-icon-plus' }
-            ],
-            onClick: function (event) {
-                if (event.target == 'add') {
-                    w2ui.itemgrid.add({ recid: w2ui.itemgrid.records.length + 1 });
-                }
-            }
-        },
         records: [
         ]
     }
 }
 
 
-function openItemPopup(recid) {
+
+function p_itemgrid(recid) {
+    return { 
+        name: 'p_itemgrid', 
+        show: {
+            toolbar: true,
+            toolbarDelete: true,
+            toolbarSave: true,
+        },
+//        url: '/'+recid,
+
+        columns: [                
+            { field: 'nombre', caption: 'nombre', size: '120px', sortable: true, resizable: true, },
+            { field: 'desc', caption: 'descripción', size: '50%', sortable: true, resizable: true, },
+            { field: 'presentacion', caption: 'presentación', size: '100px', sortable: true, resizable: true, },
+            { field: 'precio', caption: 'precio', size: '80px', resizable: true, render: 'money' },
+            { field: 'cantidad', caption: 'cantidad', size: '80px', resizable: true, render: 'float:3',
+              editable: { type: 'float' } },
+        ],
+        records: [
+            { recid: 1, nombre: "jitomate", desc: 'hidroponico', presentacion: 'kg', cantidad: 2.2 },
+        ]
+    }
+}
+
+
+
+
+function openOfertaItemPopup(recid) {
     w2popup.open({
         title   : 'items en la oferta '+recid,
         width   : 600,
@@ -203,7 +221,27 @@ function openItemPopup(recid) {
         body    : '<div id="poplayout" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
         onOpen  : function (event) {
             event.onComplete = function () {
-                $('#w2ui-popup #poplayout').w2render('itemgrid');
+                $('#w2ui-popup #poplayout').w2render('o_itemgrid');
+            };
+        },
+        onToggle: function (event) { 
+            event.onComplete = function () {
+                w2ui.layout.resize();
+            }
+        }
+    });
+}
+
+
+function openPedidoItemPopup(recid) {
+    w2popup.open({
+        title   : 'ordena contra '+recid,
+        width   : 600,
+        height  : 600,
+        body    : '<div id="poplayout" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
+        onOpen  : function (event) {
+            event.onComplete = function () {
+                $('#w2ui-popup #poplayout').w2render('p_itemgrid');
             };
         },
         onToggle: function (event) { 
@@ -216,8 +254,11 @@ function openItemPopup(recid) {
 
 
 
+
+
+
+// initialization
 $(function () {
-    // initialization
     $('#main').w2layout(config.layout);
     w2ui.layout.content('left', $().w2sidebar(config.sidebar));
     w2ui.layout.content('main', $().w2grid(config.grid1));
