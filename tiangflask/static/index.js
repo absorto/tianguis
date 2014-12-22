40f78468d48e6b800a70aa471c4ca37a96953ce5
Rodrigo García <absorto@sdf.org>
2014-12-22 14:08:14 -0600

    me deshago de la variable config

--- tiangflask/static/index.js
+++ tiangflask/static/index.js
@@ -8,167 +8,164 @@ w2utils.locale('bower_components/w2ui/locale/es-mx.json');
 // # /pedidos/inbox
 // # /pedidos/mercado
 
-// widget configuration
+
 var unidades = ['kg', 'bolsa', 'ramo', 'litro', 'domo', 'manojo', 'pieza'];
 
-var config = {
+////////////////
+// top layout //
+////////////////
+var layout = { name: 'layout',
+               padding: 0,
+               panels: [
+                 { type: 'left', size: 200, resizable: true, minSize: 120 },
+                 { type: 'main', minSize: 550, overflow: 'hidden' }
+               ]
+             };
 
-    ////////////////////
-    // layout general //
-    ////////////////////
-    layout: {
-        name: 'layout',
-        padding: 0,
-        panels: [
-            { type: 'left', size: 200, resizable: true, minSize: 120 },
-            { type: 'main', minSize: 550, overflow: 'hidden' }
-        ]
-    },
 
-    /////////////
-    // sidebar, va en left //
-    /////////////
-    sidebar: {
-        name: 'sidebar',
-        nodes: [ 
-            { id: 'ofertas', text: 'Ofertas', group: true, expanded: true, nodes: [
-                { id: 'o_mias', text: 'Mías', img: ' w2ui-icon-columns', selected: true },
-                { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
-                { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' }
-            ]},
-            { id: 'pedidos', text: 'Pedidos', group: true, expanded: true, nodes: [
-                { id: 'p_mias', text: 'Mis pedidos', img: 'w2ui-icon-columns' },
-                { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
-                { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' }
-            ]}
 
-        ],
-        onClick: function (event) {
-            switch (event.target) {
-                case 'o_mias':
+/////////////////////////////////
+// menu general a la izquierda //
+/////////////////////////////////
+var sidebar = { name: 'sidebar',
+                nodes: [ 
+                  { id: 'ofertas', text: 'Ofertas', group: true, expanded: true, nodes: [
+                    { id: 'o_mias', text: 'Mías', img: ' w2ui-icon-columns', selected: true },
+                    { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
+                    { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' }
+                  ]},
+                  { id: 'pedidos', text: 'Pedidos', group: true, expanded: true, nodes: [
+                    { id: 'p_mias', text: 'Mis pedidos', img: 'w2ui-icon-columns' },
+                    { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
+                    { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' }
+                  ]}
+
+                ],
+                onClick: function (event) {
+                  switch (event.target) {
+                    case 'o_mias':
                     w2ui.layout.content('main', w2ui.o_mias);
                     break;
-               case 'o_inbox':
-                    $().w2grid(config.o_inbox);
+                    case 'o_inbox':
+                    $().w2grid(o_inbox);
                     w2ui.layout.content('main', w2ui.o_inbox);
                     break;
-               case 'o_mercado':
+                    case 'o_mercado':
                     //w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
                     $().w2grid(config.o_inbox);
                     w2ui.layout.content('main', w2ui.o_inbox);
                     break;
-
-                case 'p_mias':
+                    
+                    case 'p_mias':
                     w2ui.layout.content('main', w2ui.grid1);
                     break;
-               case 'p_inbox':
+                    case 'p_inbox':
                     $().w2grid(config.o_inbox);
                     w2ui.layout.content('main', w2ui.o_inbox);
                     break;
-               case 'p_mercado':
+                    case 'p_mercado':
                     //w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
                     $().w2grid(config.o_inbox);
                     w2ui.layout.content('main', w2ui.o_inbox);
-
-              
-            }
-        }
-    },
-
-
-    /////////////
-    // ofertas //
-    /////////////
-    o_mias: {
-        name: 'o_mias',
-        url: '/ofertas/mias',
-//        method: 'GET', 
-        show: {
-            toolbar : true,
-            toolbarDelete: true,
-            toolbarSave: false,
-            toolbarEdit: true
-        },
-        toolbar: {
-            items: [
-                { id: 'add', type: 'button', caption: 'Nueva oferta', icon: 'w2ui-icon-plus' },
-                { type: 'break',  id: 'break0' },
-                { id: 'save', type: 'button', caption: 'Guardar', icon: 'w2ui-icon-check' }
-            ],
-            onClick: function (event) {
-                if (event.target == 'add') {
-                    console.log('dafuq');
-                    var newid = w2ui.o_mias.records.length + 1;
-                    w2ui.o_mias.add({ recid: newid });
-                    w2ui.o_mias.select( newid );
+                  }
                 }
-                if (event.target == 'save') {
-                    //console.log(w2ui.o_mias.records);
-                    w2ui.o_mias.mergeChanges();                     
-                    $.ajax({
-                        type: "POST",
-                        url: "/ofertas/save",
-                        data: JSON.stringify( w2ui.o_mias.records ),
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json"
-                    });
-                }
-            }
+              };
 
-        },
 
-        columns: [
-            { field: 'titulo', caption: 'Título', size: '180px', editable: { type: 'text' } },
-            { field: 'desc', caption: 'Descripción', size: '180px', editable: { type: 'text' } },
-            { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true, editable: { type: 'date' }},
-            { field: 'publicado', caption: 'Publicado', size: '100px', sortable: true, resizable: true, 
-                editable: { type: 'checkbox' } 
-            },
-        ],
 
-        searches: [
-            { type: 'date', field: 'vigencia', caption: 'Vigencia' }
-        ],
+////////////////////////////////////////////
+// grid con mis ofertas
+////////////////////////////////////////////
+var o_mias = {
+  name: 'o_mias',
+  url: '/ofertas/mias',
 
-        // abre lista de items
-        onEdit: function(event) {
-            $().w2grid(o_itemgrid(event.recid));
-            openOfertaItemPopup(event.recid);
-        },
-    
-    },
+  show: {
+    toolbar : true,
+    toolbarDelete: true,
+    toolbarSave: false,
+    toolbarEdit: true
+  },
 
-    /////////////
-    // inbox //
-    /////////////
-  o_inbox: { 
-        name: 'o_inbox',
-                show: {
-                    toolbar : true
-                },
-        searches: [
-            { type: 'date', field: 'vigencia', caption: 'Vigencia' }
-        ],
+  toolbar: {
+    items: [
+      { id: 'add', type: 'button', caption: 'Nueva oferta', icon: 'w2ui-icon-plus' },
+      { type: 'break',  id: 'break0' },
+      { id: 'save', type: 'button', caption: 'Guardar', icon: 'w2ui-icon-check' }
+    ],
+    onClick: function (event) {
+      if (event.target == 'add') {
+        console.log('dafuq');
+        var newid = w2ui.o_mias.records.length + 1;
+        w2ui.o_mias.add({ recid: newid });
+        w2ui.o_mias.select( newid );
+      }
+      if (event.target == 'save') {
+        //console.log(w2ui.o_mias.records);
+        w2ui.o_mias.mergeChanges();                     
+        $.ajax({
+          type: "POST",
+          url: "/ofertas/save",
+          data: JSON.stringify( w2ui.o_mias.records ),
+          contentType: "application/json; charset=utf-8",
+          dataType: "json"
+        });
+      }
+    }  
+  }, // toolbar
 
-        columns: [
-            { field: 'usuario', caption: 'Usuario', size: '180px' },
-            { field: 'titulo', caption: 'Título', size: '180px' },
-            { field: 'desc', caption: 'Descripción', size: '180px' },
-            { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true },
-            { field: 'lugar', caption: 'Lugar', size: '180px'}
-        ],
-
-        onClick: function (event) {
-            $().w2grid(p_itemgrid(event.recid));
-            openPedidoItemPopup(event.recid);
-        },
-        
-      records: [
-          { recid: 1, usuario: "la granja", titulo: 'puesto', desc: 'Tianguis el 100', email: 'jdoe@gmail.com', vigencia: '4/3/2012', lugar: 'mercado100' },
-          { recid: 2, usuario: "la nicolasa", titulo: 'A domicilio', desc: 'fruta y verdura otoñal', email: 'jdoe@gmail.com', vigencia: '4/3/2012', lugar: 'indefinido' }
-        ]
+  columns: [
+    { field: 'titulo', caption: 'Título', size: '180px', editable: { type: 'text' } },
+    { field: 'desc', caption: 'Descripción', size: '180px', editable: { type: 'text' } },
+    { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true, editable: { type: 'date' }},
+    { field: 'publicado', caption: 'Publicado', size: '100px', sortable: true, resizable: true, editable: { type: 'checkbox' } 
     }
-}
+  ],
+
+  searches: [
+    { type: 'date', field: 'vigencia', caption: 'Vigencia' }
+  ],
+
+  // abre lista de items
+  onEdit: function(event) {
+    $().w2grid(o_itemgrid(event.recid));
+    openOfertaItemPopup(event.recid);
+  }
+          
+}; // o_mias
+
+
+
+
+var o_inbox = { 
+  name: 'o_inbox',
+  show: {
+    toolbar : true
+  },
+  searches: [
+    { type: 'date', field: 'vigencia', caption: 'Vigencia' }
+  ],
+  url: '/ofertas/inbox',
+  columns: [
+    { field: 'usuario', caption: 'Usuario', size: '180px' },
+    { field: 'titulo', caption: 'Título', size: '180px' },
+    { field: 'desc', caption: 'Descripción', size: '180px' },
+    { field: 'vigencia', caption: 'Vigencia', size: '120px', sortable: true },
+    { field: 'lugar', caption: 'Lugar', size: '180px'}
+  ],
+
+  onClick: function (event) {
+    $().w2grid(p_itemgrid(event.recid));
+    openPedidoItemPopup(event.recid);
+  },
+        
+  records: [
+    { recid: 1, usuario: "la granja", titulo: 'puesto', desc: 'Tianguis el 100', email: 'jdoe@gmail.com', vigencia: '4/3/2012', lugar: 'mercado100' },
+    { recid: 2, usuario: "la nicolasa", titulo: 'A domicilio', desc: 'fruta y verdura otoñal', email: 'jdoe@gmail.com', vigencia: '4/3/2012', lugar: 'indefinido' }
+  ]
+};
+
+
 
 
 
@@ -288,9 +285,9 @@ function openPedidoItemPopup(recid) {
 
 // initialization
 $(function () {
-    $('#main').w2layout(config.layout);
-    w2ui.layout.content('left', $().w2sidebar(config.sidebar));
-    w2ui.layout.content('main', $().w2grid(config.o_mias));
+    $('#main').w2layout(layout);
+    w2ui.layout.content('left', $().w2sidebar(sidebar));
+    w2ui.layout.content('main', $().w2grid(o_mias));
     w2ui.o_mias.autoLoad = false;
     w2ui.o_mias.skip(0);
 });
