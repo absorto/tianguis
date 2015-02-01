cfdcf7aeef20f8e9b881b8ef15db11bce63bd88d
absorto <absorto@sdf.org>
2015-01-31 19:55:26 -0600

    heavy refactoring goin on

--- tiangflask/static/js/layout.js
+++ tiangflask/static/js/layout.js
@@ -1,57 +1,23 @@
 ////////////////
 // top layout //
 ////////////////
+
+var pstyle = 'background-color: #F5F6F7; border: 1px solid #dfdfdf; padding: 5px;';
+
 var layout = { name: 'layout',
                padding: 0,
                panels: [
-                 { type: 'left', size: 200, resizable: true, minSize: 120 },
-                 { type: 'main', minSize: 550, overflow: 'hidden' }
+            { type: 'top',  size: 50, resizable: true, style: pstyle, content: 'top' },
+            { type: 'left', size: 200, resizable: true, style: pstyle, content: 'left' },
+            { type: 'main', style: pstyle, content: 'main' },
+            { type: 'preview', size: '50%', resizable: true, style: pstyle, content: "<div id='editor'> <textarea></textarea> </div>" },
+//            { type: 'right', size: 200, resizable: true, style: pstyle, content: 'right' },
+//            { type: 'bottom', size: 50, resizable: true, style: pstyle, content: 'bottom' }
+                 
+                 // { type: 'left', size: 200, resizable: true, minSize: 120 },
+                 // { type: 'left', size: 200, resizable: true, minSize: 120 },
+                 // { type: 'main', minSize: 550, overflow: 'hidden' },
+                 // { type: 'preview', minSize: 550, overflow: 'hidden' }
                ]
              };
 
-/////////////////////////////////
-// menu general a la izquierda //
-/////////////////////////////////
-var sidebar = { name: 'sidebar',
-                nodes: [ 
-                  { id: 'ofertas', text: 'Ofertas', group: true, expanded: true, nodes: [
-                    { id: 'o_mias', text: 'Mías', img: ' w2ui-icon-columns', selected: true },
-                    { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
-                    { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' }
-                  ]},
-                  { id: 'pedidos', text: 'Pedidos', group: true, expanded: true, nodes: [
-                    { id: 'p_mias', text: 'Mis pedidos', img: 'w2ui-icon-columns' },
-                    { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
-                    { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' }
-                  ]}
-
-                ],
-                onClick: function (event) {
-                  switch (event.target) {
-                    case 'o_mias':
-                    w2ui.layout.content('main', w2ui.o_mias);
-                    break;
-                    case 'o_inbox':
-                    $().w2grid(o_inbox);
-                    w2ui.layout.content('main', w2ui.o_inbox);
-                    break;
-                    case 'o_mercado':
-                    //w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
-                    $().w2grid(config.o_inbox);
-                    w2ui.layout.content('main', w2ui.o_inbox);
-                    break;
-                    
-                    case 'p_mias':
-                    w2ui.layout.content('main', w2ui.grid1);
-                    break;
-                    case 'p_inbox':
-                    $().w2grid(config.o_inbox);
-                    w2ui.layout.content('main', w2ui.o_inbox);
-                    break;
-                    case 'p_mercado':
-                    //w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
-                    $().w2grid(config.o_inbox);
-                    w2ui.layout.content('main', w2ui.o_inbox);
-                  }
-                }
-              };
--- tiangflask/static/js/ofertas.js
+++ tiangflask/static/js/ofertas.js
@@ -1,60 +1,60 @@
-function openOfertaItemPopup(recid) {
-    w2popup.open({
-        title   : 'items en la oferta '+recid,
-        width   : 900,
-        height  : 600,
-        body    : '<div id="poplayout" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
-        onOpen  : function (event) {
-            event.onComplete = function () {
-                $('#w2ui-popup #poplayout').w2render('o_itemgrid');
-            };
-        },
-        onToggle: function (event) { 
-            event.onComplete = function () {
-                w2ui.layout.resize();
-            }
-        }
-    });
-}
+// function openOfertaItemPopup(recid) {
+//     w2popup.open({
+//         title   : 'items en la oferta '+recid,
+//         width   : 900,
+//         height  : 600,
+//         body    : '<div id="poplayout" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
+//         onOpen  : function (event) {
+//             event.onComplete = function () {
+//                 $('#w2ui-popup #poplayout').w2render('o_itemgrid');
+//             };
+//         },
+//         onToggle: function (event) { 
+//             event.onComplete = function () {
+//                 w2ui.layout.resize();
+//             }
+//         }
+//     });
+// }
 
 
-function o_itemgrid(recid) {
-    return { 
-        name: 'o_itemgrid', 
-        show: {
-            toolbar: true,
-            toolbarDelete: true,
-            toolbarSave: true
-        },
-//        url: '/'+recid,
+// function o_itemgrid(recid) {
+//     return { 
+//         name: 'o_itemgrid', 
+//         show: {
+//             toolbar: true,
+//             toolbarDelete: true,
+//             toolbarSave: true
+//         },
+// //        url: '/'+recid,
 
-        toolbar: {
-            items: [
-                { id: 'add', type: 'button', caption: 'Otro item', icon: 'w2ui-icon-plus' }
-            ],
-            onClick: function (event) {
-                if (event.target == 'add') {
-                    w2ui.o_itemgrid.add({ recid: w2ui.o_itemgrid.records.length + 1 });
-                }
-            }
-        },
+//         toolbar: {
+//             items: [
+//                 { id: 'add', type: 'button', caption: 'Otro item', icon: 'w2ui-icon-plus' }
+//             ],
+//             onClick: function (event) {
+//                 if (event.target == 'add') {
+//                     w2ui.o_itemgrid.add({ recid: w2ui.o_itemgrid.records.length + 1 });
+//                 }
+//             }
+//         },
 
-        columns: [                
-            { field: 'text', caption: 'nombre', size: '120px', sortable: true, resizable: true, 
-              editable: { type: 'text' }
-            },
-            { field: 'desc', caption: 'descripción', size: '50%', sortable: true, resizable: true, 
-              editable: { type: 'text' }
-            },
-            { field: 'presentacion', caption: 'presentación', size: '100px', sortable: true, resizable: true, 
-              editable: { type: 'combo', items: unidades, openOnFocus: true } 
-            },
-            { field: 'precio', caption: 'precio', size: '80px', sortable: true, resizable: true, render: 'money',
-              editable: { type: 'money' }
-            },
-        ],
-        records: [
-        ]
-    }
-}
+//         columns: [                
+//             { field: 'text', caption: 'nombre', size: '120px', sortable: true, resizable: true, 
+//               editable: { type: 'text' }
+//             },
+//             { field: 'desc', caption: 'descripción', size: '50%', sortable: true, resizable: true, 
+//               editable: { type: 'text' }
+//             },
+//             { field: 'presentacion', caption: 'presentación', size: '100px', sortable: true, resizable: true, 
+//               editable: { type: 'combo', items: unidades, openOnFocus: true } 
+//             },
+//             { field: 'precio', caption: 'precio', size: '80px', sortable: true, resizable: true, render: 'money',
+//               editable: { type: 'money' }
+//             },
+//         ],
+//         records: [
+//         ]
+//     }
+// }
 
