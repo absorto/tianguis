258ce385d34ce0d7b3eedfbc0dee15adb5b3166d
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-03-29 16:57:31 -0600

    sidebar para contactos

--- tiangflask/static/js/sidebar.js
+++ tiangflask/static/js/sidebar.js
@@ -3,16 +3,26 @@
 /////////////////////////////////
 var sidebar = { name: 'sidebar',
                 nodes: [ 
-                  { id: 'ofertas', text: 'Ofertas', group: true, expanded: true, nodes: [
-                    { id: 'o_mias', text: 'Mías', img: ' w2ui-icon-columns', selected: true },
-                    { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
-                    { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' }
-                  ]},
-                  { id: 'pedidos', text: 'Pedidos', group: true, expanded: true, nodes: [
-                    { id: 'p_mias', text: 'Mis pedidos', img: 'w2ui-icon-columns' },
-                    { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
-                    { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' }
-                  ]}
+                  { id: 'ofertas', text: 'Ofertas', group: true, expanded: true,
+                    nodes: [
+                      { id: 'o_mias', text: 'Mías', img: ' w2ui-icon-columns', selected: true },
+                      { id: 'o_inbox', text: 'Inbox', img: 'icon-folder' },
+                      { id: 'o_mercado', text: 'En el mercado', img: 'icon-page' }
+                    ]},
+                  { id: 'pedidos', text: 'Pedidos', group: true, expanded: true,
+                    nodes: [
+                      { id: 'p_mias', text: 'Mis pedidos', img: 'w2ui-icon-columns' },
+                      { id: 'p_inbox', text: 'Inbox', img: 'icon-folder' },
+                      { id: 'p_mercado', text: 'En el mercado', img: 'icon-page' }
+                    ]
+                  },
+                  { id: 'contactos_sidebar', text: 'Contactos', group: true, expanded: true,
+                    nodes: [
+                      { id: 'contactos', text: 'Contactos', img: 'w2ui-icon-columns' },
+                      { id: 'listas', text: 'Listas', img: 'icon-folder' },
+                    ]
+                  },
+                  
 
                 ],
                 onClick: function (event) {
@@ -42,6 +52,14 @@ var sidebar = { name: 'sidebar',
                     //w2ui.layout.content('main', "la patita se ha enojado ya sabes por qué");
                     $().w2grid(config.o_inbox);
                     w2ui.layout.content('main', 'w2ui.o_inbox');
+
+                  case 'contactos_sidebar':
+                    w2ui.layout.content('main', $().w2grid(contactos));
+                    w2ui.layout.content('preview', 'preview oferta');
+                    break;
+
+                    
                   }
+                  
                 }
               };
