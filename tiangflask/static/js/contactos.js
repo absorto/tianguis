38b5d440ab6914574708ccd045b6b8698f304918
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-04-12 22:42:45 -0500

    indentación chida

--- tiangflask/static/js/contactos.js
+++ tiangflask/static/js/contactos.js
@@ -1,35 +1,37 @@
-contactos_grid = { 
-        name: 'contactos_grid', 
-        show: { 
-            toolbar: true,
-            footer: true,
-            toolbarSave: true
-        },
-        columns: [                
-          { field: 'nombre', caption: 'Nombre', size: '30%', sortable: true, resizable: true, 
-            editable: { type: 'text' }
-          },
-          { field: 'email', caption: 'Correo', size: '30%', sortable: true, resizable: true,
-            editable: { type: 'email' }
-          },
-          { field: 'phone', caption: 'Teléfono', size: '20%', sortable: false, resizable: true,
-            editable: { type: 'text' }
-          },
-          { field: 'etc', caption: '&c', size: '20%', sortable: false, resizable: true,
-            editable: { type: 'text' }
-          }
-        ],
-        toolbar: {
-            items: [
-                { id: 'add', type: 'button', caption: 'Add Record', icon: 'w2ui-icon-plus' }
-            ],
+contactos_grid = {
+  url: '/contactos',
+  name: 'contactos_grid', 
+  show: { 
+    toolbar: true,
+    footer: true,
+    toolbarSave: true,
+    toolbarDelete: true          
+  },
+  columns: [                
+    { field: 'nombre', caption: 'Nombre', size: '30%', sortable: true, resizable: true, 
+      editable: { type: 'text' }
+    },
+    { field: 'email', caption: 'Correo', size: '30%', sortable: true, resizable: true,
+      editable: { type: 'email' }
+    },
+    { field: 'phone', caption: 'Teléfono', size: '20%', sortable: false, resizable: true,
+      editable: { type: 'text' }
+    },
+    { field: 'etc', caption: '&c', size: '20%', sortable: false, resizable: true,
+      editable: { type: 'text' }
+    }
+  ],
+  toolbar: {
+    items: [
+      { id: 'add', type: 'button', caption: 'Agregar contacto', icon: 'w2ui-icon-plus' }
+    ],
           
-            onClick: function (event) {
-                if (event.target == 'add') {
-                    w2ui.contactos_grid.add({ recid: w2ui.contactos_grid.records.length + 1 });
-                }
-            }
-        }
+    onClick: function (event) {
+      if (event.target == 'add') {
+        w2ui.contactos_grid.add({ recid: w2ui.contactos_grid.records.length + 1 });
+      }
+    }
+  }
 };
 
 
